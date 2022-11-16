2fafa145db3226178d18002ff56f6ead26200f81
saliha laidi <saliha.laidi@hotmail.com>
2022-11-16 14:40:17 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  resources :restaurants do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    resources :reviews, only: %i[new create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+tante_charlotte = {name: "Tante Charlotte", address: "Quartier St Pierre, Bordeaux", category: "french"}
+escale_des_pirates =  {name: "Escale des Pirates", address: "Proche aéroport, Mérignac", category: "japanese"}
+entrecote = {name: "Entrecote", address: "Place Tourny, Bordeaux", category: "belgian"}
+bistrot_regent = {name: "Bistrot Régent", address: "Avenue Aquitaine, Ste Eulalie", category: "italian"}
+sushi_shop = {name: "Sushi Shop", address: "Cours St Louis, Bordeaux", category: "chinese"}
+
+[tante_charlotte, escale_des_pirates, entrecote, bistrot_regent,sushi_shop].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
